d7344d569b111af485605ac4b5d634e1cdaa086e
amercado <arnold.mercado@hotmail.com>
2024-12-22 15:04:02 +0800

    Implement Home Screen List

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {Text, View, Image, TouchableOpacity, ScrollView} from "react-native";
+import {Text, View, Image, TouchableOpacity, ScrollView, FlatList} from "react-native";
 import {Link} from "expo-router";
 import {SafeAreaView} from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -9,31 +9,44 @@ import {Card, FeaturedCard} from "@/components/Cards";
 import SummaryHeader from "@/components/SummaryHeader";
 import Filters from "@/components/Filters";
 import TopBar from "@/components/TopBar";
+import useGlobalContext from "@/lib/global-provider";
 
 
 export default function Index() {
 
+
     return (
         <SafeAreaView className={"bg-light-900 h-full"}>
-            <TopBar />
-            <View className={"px-5"}>
-                <Search/>
-                <ScrollView>
-                    <SummaryHeader header="Featured" linkTitle="See All"/>
-                    <View className={"flex flex-row gap-5 mt-5"}>
-                        <FeaturedCard/>
-                        <FeaturedCard/>
-                        <FeaturedCard/>
-                    </View>
-                    <SummaryHeader header="Our Recommendation" linkTitle="See All"/>
-                    <Filters />
-                    <View className={"flex flex-row gap-5 mt-5"}>
-                        <Card/>
-                        <Card/>
-                    </View>
-                </ScrollView>
+            <FlatList
+                contentContainerClassName={"pb-32"}
+                columnWrapperClassName={"flex gap-5 px-5"}
+                showsVerticalScrollIndicator={false}
+                data={[1, 2]}
+                renderItem={({item}) => (
+                    <Card/>
+                )}
+                keyExtractor={(item) => item.toString()}
+                numColumns={2}
+                ListHeaderComponent={() => (
+                    <>
+                        <TopBar/>
+                        <View className={"px-5"}>
+                            <Search/>
+                            <SummaryHeader header={"Featured"} linkTitle={"Show All"}/>
+                            <FlatList
+                                data={[1, 2,3 ]}
+                                bounces={false}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerClassName={"flex gap-5 mt-5"}
+                                horizontal={true}
+                                keyExtractor={(item) => item.toString()}
+                                renderItem={({item}) => <FeaturedCard />}/>
+                        </View>
+                    </>
+                )}
+            />
 
-            </View>
+
         </SafeAreaView>
     );
 }
