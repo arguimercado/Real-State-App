9fa6882152dc0dba98074b01172f54aded167abe
amercado <arnold.mercado@hotmail.com>
2024-12-22 16:22:08 +0800

    Appwrite Database Setup

--- lib/persistence/appwrite.ts
+++ lib/persistence/appwrite.ts
@@ -1,11 +1,16 @@
 
 import * as Linking from "expo-linking";
-import {OAuthProvider,Client,Avatars,Account} from "react-native-appwrite";
+import {OAuthProvider, Client, Avatars, Account, Databases} from "react-native-appwrite";
 import {openAuthSessionAsync} from "expo-web-browser";
 export const config = {
     platform: "com.myapp.restate",
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 
 export const client = new Client();
@@ -18,6 +23,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
     try {
